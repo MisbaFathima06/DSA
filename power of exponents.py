7ac46cd32f8301910b41b0ece3a3b3002d068428
Misba Fathima <134159136+MisbaFathima06@users.noreply.github.com>
2025-09-21 03:45:18 +0000

    Binary exponention

--- power of exponents.py
+++ power of exponents.py
@@ -1,9 +1,30 @@
 x=int(input("Enter the x value: "))
-y=int(input("Enter the y value: "))
+n=int(input("Enter the n value: "))
 
-def pow(x,y):
-    if y==0:
+# 1.Using Recursion
+def pow(x,n):
+    if n==0:
         return 1
-    return x**(y-1)*x
+    return x**(n-1)*x
 
-print(pow(x,y))
+print(pow(x,n))
+
+# 2. Divide and Conquer Rule
+def power(x,n):
+        # Base cases
+    if n==0:
+        return 1
+    elif x==0:
+        return 0
+        
+    half= power(x,n//2)
+
+    if n%2==0:
+        return half*half
+    else:
+        return x* half*half
+
+print(power(x,n))
+        
+
+
